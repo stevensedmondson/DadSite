54293c33dddc9db26015fba78b3f7a7540b60389
Steve Edmondson <stevensedmondson@gmail.com>
2024-03-06 11:33:50 -0500

    e

--- dadsite/src/app/services/page.tsx
+++ dadsite/src/app/services/page.tsx
@@ -1,61 +1,60 @@
+import Image from "next/image"
+
 
 export default function Services() {
     return (
-        <div className="mb-32 grid mt-10 content-center text-center lg:max-w-[1140px] lg:w-full lg:mb-24 lg:grid-cols-3">
-            <div className="flex justify-center">
-                <a
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 "
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        FAA Medical Exams{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                        -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Find in-depth information about Next.js features and API.
-                    </p>
-                </a>
+        <div className="">    
+            <div>    
+                <div className="flex font-semibold w-full lg:w-auto h-auto flex-col items-center">
+                    <div className="max-w-[90vw]">
+                        <Image 
+                            src="/stock2.jpeg"
+                            width={1140}
+                            height={500}
+                            alt=""
+                        />
+                    </div>
+                </div>
             </div>
-            <div className="flex justify-center">
-                <a
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                <h2 className={`mb-3 text-2xl font-semibold`}>
-                    EKG{" "}
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                    </span>
-                </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                    Learn about Next.js in an interactive course with&nbsp;quizzes!
-                </p>
-                </a>
-            </div>
-            <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-                Special Issuance{" "}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-                </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Explore starter templates for Next.js.
-            </p>
-            </a>
 
-            
+            <div className="flex justify-center uppercase text-[#07668C] font-semibold font-calibri text-4xl pt-12">Our Services</div>
+
+            <div className="flex max-lg:flex-col items-center justify-items-center lg:justify-center w-full pt-10">    
+                <div className="lg:mx-5 max-w-[500px] min-w-[300px]">
+                    <Image 
+                        src="/stock1.jpeg"
+                        width={1000}
+                        height={600}
+                        alt=""
+                    />
+                    <h1 className="flex justify-center">
+                        FAA Medical Exams
+                    </h1>
+                </div>
+                <div className="lg:mx-5 max-w-[500px] min-w-[300px]">
+                    <Image 
+                        src="/ekg.jpeg"
+                        width={1000}
+                        height={600}
+                        alt=""
+                    />
+                    <h1 className="flex justify-center">
+                        EKG
+                    </h1>
+                </div>
+                <div className="lg:mx-5 max-w-[500px] min-w-[300px]">
+                    <Image 
+                        src="/stock3.jpeg"
+                        width={1000}
+                        height={600}
+                        alt=""
+                    />
+                    <h1 className="flex justify-center">
+                        Special Issuance
+                    </h1>
+                </div>
+            </div>
         </div>
+        
     )
 }
