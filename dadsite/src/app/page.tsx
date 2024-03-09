40250edefbf18612d29350b206255ef8039ed2ae
Steve Edmondson <stevensedmondson@gmail.com>
2024-03-08 21:07:34 -0500

    modified:   dadsite/src/app/page.tsx

--- dadsite/src/app/page.tsx
+++ dadsite/src/app/page.tsx
@@ -20,10 +20,10 @@ export default function Home() {
           Welcome to Pilot Medical Exams, PLLC, your trusted destination for aviation medical services conveniently located right at Gerald R Ford Airport in Grand Rapids, MI.
         </h3>
         <h3 className="pt-[2rem] text-xl font-normal">
-          We offer a range of services including AME medical exams, EKG screenings, and assistance with Special Issuance  cases. Our office is conveniently situated inside the Amway Aviation Hangar, making it easy for pilots to access   essential medical care right at the airport.
+          We offer a range of services including AME medical exams, EKG screenings, and assistance with Special Issuance  cases. Our office is conveniently situated inside the Amway Aviation Hangar, making it easy for pilots to access essential medical care right at the airport.
         </h3>
       </div>
-      <div className="bg-[url('/hangar1.jpg')] bg-cover bg-no-repeat w-full mt-10 p-[5rem] text-[#044159]">
+      <div className="bg-[url(/hangar1.jpg)] bg-cover bg-no-repeat w-full mt-10 p-[5rem] text-[#044159]">
         <div className="bg-[#D5E7F2]/90 p-5 rounded-2xl shadow-2xl mx-[20vw]">
           <h1 className="flex justify-center uppercase text-4xl pt-5">Testimonials</h1>
           <p className="flex text-center pt-10 pb-5 font-medium">
