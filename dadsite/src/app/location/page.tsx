87368c2eb3783fc752cd6949b3b94273024a3550
Steve Edmondson <stevensedmondson@gmail.com>
2024-04-26 01:46:09 -0400

    z

--- dadsite/src/app/location/page.tsx
+++ dadsite/src/app/location/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Location() {
     return (
-        <div className="p-10 w-full]">    
+        <div className="p-10 w-full">    
             <div>   
                 <h1 className="flex justify-center uppercase font-semibold font-calibri text-5xl pt-24 text-white">Location</h1>
             </div>
